8edc00105bfa9e40480eb623e643d5b845d8430a
HernandezLivia <joel.sw26@gmail.com>
2025-04-15 23:17:35 -0500

    creacion del proyecto

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Sistema-Prestamos-Biblioteca---Java
+Java 17
+Javax.Swing + Flatlaf
---
+++ SistemaPrestamosBiblioteca/src/main/java/com/mycompany/sistemaprestamosbiblioteca/SistemaPrestamosBiblioteca.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.sistemaprestamosbiblioteca;
+
+
+public class SistemaPrestamosBiblioteca {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
